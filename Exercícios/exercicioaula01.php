9e227b7021401e0562083ef91986b3c91649e728
“Ton” <bertolassionline@gmail.com>
2022-06-05 19:00:20 -0300

    envio exercicio aulas 1 e 4

---
+++ Exercícios/exercicioaula01.php
@@ -0,0 +1,35 @@
+<form action="" method="post"> 
+
+    <input name="n1" type="text" placeholder="Número 1"> <br>
+
+    <input name="n2" type="text" placeholder="Número 2"> <br>
+
+    <button name="soma">Somar</button>
+    <button name="subtrair">Subtrair</button>
+    <button dividir="dividir">Dividir</button>
+    <button name="multiplicar">Multiplicar</button>
+
+</form>
+
+<?php
+
+    if ($_POST)  {
+        
+        if (isset($_POST['subtrair'])) {
+            echo $_POST['n1'] - $_POST['n2'];
+        }
+
+        if (isset($_POST['soma'])) {
+            echo $_POST['n1'] + $_POST['n2'];
+        }
+
+        if (isset($_POST['dividir'])) {
+            echo $_POST['n1'] / $_POST['n2'];
+        }
+
+        if (isset($_POST['multiplicar'])) {
+            echo $_POST['n1'] * $_POST['n2'];
+        }
+    }
+
+?>
